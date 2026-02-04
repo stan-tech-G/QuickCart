88102833192a074c5e3f912daa3555097aff459b
stan-tech-G <stano5rk@gmail.com>
2026-02-04 19:18:37 +0300

    Fix build and ready for Vercel

--- app/api/ingest/route.js
+++ app/api/ingest/route.js
@@ -1,5 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/config/inngest";
+import { syncUserCreation } from "@/config/inngest";
+import { syncUserUpdate } from "@/config/inngest";
+import { syncUserDeletion } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
